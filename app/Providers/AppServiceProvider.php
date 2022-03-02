39130661435ece191ef153b0fdd3407b989cbe92
mahmoud-m-abadi <mahmood.nh2008@gmail.com>
2022-03-02 18:39:26 +0330

    Fix issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if ($this->app->environment() != 'testing') {
+        if ($this->app->environment() != 'testing' AND request()->expectsJson()) {
             /** Login user */
             auth()->loginUsingId(1);
         }
